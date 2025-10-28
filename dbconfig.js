072faf43eb96650b9902bef4742ad8287942e26f
FOGOLIN LAGARES MATHIAS <48803162@ort>
2025-10-28 11:35:53 -0300

    begeeeeeeeeeeeeeeeee

--- dbconfig.js
+++ dbconfig.js
@@ -5,7 +5,7 @@
 
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize(process.env.DB_URL, {dialect: "postgres"});
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {dialect: "postgres"});
 try{
     await sequelize.authenticate();
     console.log("Conección extablecida");
